20f959af34a9971e4461998e8745104ad8218229
Anton Fletcher <Anton Fletcher anton.fletcher@netspot.com.au>
2013-11-15 15:57:04 +1030

    Actually use algorithm for 5

--- euler/python/p5.py
+++ euler/python/p5.py
@@ -1,3 +1,29 @@
-'''Highest factor of all primes'''
+import sys
+import eu
 
-print 5 * 7 * 9 * 11 * 13 * 16 * 17 * 19
+def main(n):
+	list_of_factors = {}
+
+	n = int(n[1])
+
+	for i in range(2, n + 1):
+		if eu.isPrime(i):
+			list_of_factors[i] = i
+
+	for factor in list_of_factors:
+		result = 0
+		power = 1
+		while result < n:
+			list_of_factors[factor] = result
+			result = factor ** power
+			power = power + 1
+
+	smallest_product = 1
+
+	for factor in list_of_factors:
+		smallest_product = smallest_product * list_of_factors[factor]
+
+	print smallest_product
+
+if __name__ == "__main__":
+	main(sys.argv)
